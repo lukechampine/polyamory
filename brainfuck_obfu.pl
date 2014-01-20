d39843ac07a7c9ad914ecaedaf3c20ad9fcb5532
Luke Champine <luke.champine@gmail.com>
2014-01-19 20:27:32 -0500

    shorten obfu

--- brainfuck_obfu.pl
+++ brainfuck_obfu.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # Warning: this version does not do any error checking!
-push@g,split'',$_ while<>;for(0..$#g){push@s,$_ if(@g[$_]eq"["
-);$a=pop@s,$l{$a}=$_,$l{$_}=$a if$g[$_]eq"]"}%c=(62=>'$p++',60
-=>'$p--',43=>'$a[$p]++',45=>'$a[$p]--',46=>'print chr$a[$p];',
-44=>'$a[$p]=ord(getc)',91=>'$i=$l{$i}if!$a[$p]',93=>'$i=$l{$i'
-.'}if$a[$p];');use bytes;eval$c{ord$g[$i]},$i++while($i<~~@g);
+use bytes;%c=qw(. print+chr$a[$p] + $a[$p]++ > $p++
+[ $i=$l{$i}if!$a[$p] ] $i=$l{$i}if$a[$p] - $a[$p]--
+, $a[$p]=ord(getc) < $p--);@g=split'',<>;for(0..$#g
+){push @s,$_ if$g[$_]eq"[";$a=pop@s,$l{$a}=$_,$l{$_
+}=$a if$g[$_]eq"]"}eval$c{$g[$i]},$i++while($i<$#g)
