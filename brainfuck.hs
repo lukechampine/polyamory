f7664fb81ea5f281a2c16c594585e0658a7e2f90
Luke Champine <luke.champine@gmail.com>
2014-04-14 16:25:23 -0400

    implement IO commands
    and remove the Maybes. And add a custom function for reading
    stdin, because getContents closes it.
    This is still a pretty lame implementation. It still doesn't do any
    error checking, and it's terribly slow, probably because of list
    lookups are O(n). Using a more efficient data structure should
    speed things up considerably.

--- brainfuck.hs
+++ brainfuck.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import System.IO (isEOF, hFlush, stdout)
 
 data BF = BF { program :: String
              , prgpointer :: Int
@@ -9,17 +10,11 @@ data BF = BF { program :: String
 newBF :: String -> BF
 newBF prog = BF {program = prog, prgpointer = 0, array = repeat '\0', pointer = 0}
 
-curSym :: BF -> Maybe Char
-curSym bf
-	| prgpointer bf < 0 = Nothing
-	| prgpointer bf >= (length . program) bf = Nothing
-	| otherwise = Just (program bf !! prgpointer bf)
+curSym :: BF -> Char
+curSym bf = program bf !! prgpointer bf
 
-curVal :: BF -> Maybe Char
-curVal bf
-	| pointer bf < 0 = Nothing
-	| pointer bf >= (length . array) bf = Nothing
-	| otherwise = Just (array bf !! pointer bf)
+curVal :: BF -> Char
+curVal bf = array bf !! pointer bf
 
 adv :: BF -> BF
 adv bf = bf {prgpointer = prgpointer bf + 1}
@@ -29,56 +24,54 @@ rec bf = bf {prgpointer = prgpointer bf - 1}
 
 jump :: BF -> BF
 jump bf
-	| sym == Just '[' && val == Just '\0' = jmpTo ']' (adv bf)
-	| sym == Just ']' && val /= Just '\0' = jmpTo '[' (rec bf)
-	| otherwise                      = bf
+	| sym == '[' && val == '\0' = jmpTo ']' (adv bf)
+	| sym == ']' && val /= '\0' = jmpTo '[' (rec bf)
+	| otherwise                 = bf
 	where
 		(sym, val) = (curSym bf, curVal bf)
 
 jmpTo :: Char -> BF -> BF
 jmpTo c bf
-	| curSym bf == Nothing      = bf {prgpointer = -2}
-	| curSym bf == Just (opp c) = (jmpTo c . jmpTo c) bf
-	| curSym bf == Just c       = bf
-	| otherwise                 = (jmpTo c . rec) bf
+	| curSym bf == (opp c) = jmpTo c . mov c . jmpTo c . mov c $ bf
+	| curSym bf == c       = bf
+	| otherwise            = jmpTo c . mov c $ bf
 	where
-		opp '[' = ']'
-		opp ']' = '['
+		opp '[' = ']'; opp ']' = '['
+		mov '[' = rec; mov ']' = adv
 
-next :: BF -> BF
-next bf = bf {pointer = pointer bf + 1}
+setCell :: Char -> BF -> BF
+setCell c bf = let (l,r) = splitAt (pointer bf) (array bf)
+               in bf {array = l ++ [c] ++ tail r}
 
-prev :: BF -> BF
-prev bf = bf {pointer = pointer bf - 1}
+modChar :: Int -> (Char -> Char)
+modChar n = chr . (`mod` 256) . (+ n) . ord
 
-updateElem :: Int -> Int -> [Char] -> [Char]
-updateElem n s xs = let (l,r) = splitAt n xs
-               in l ++ [chr (ord (head r) + s)] ++ tail r
-
-inc :: BF -> BF
-inc bf = bf {array = updateElem (pointer bf) 1 (array bf)}
-
-dec :: BF -> BF
-dec bf = bf {array = updateElem (pointer bf) (-1) (array bf)}
-
-interpret :: BF -> BF
+interpret :: BF -> IO ()
 interpret bf =
-	if curSym bf == Nothing
-	then bf
-	else interpret . adv $ case curSym bf of
-		Just '>' -> next bf
-		Just '<' -> prev bf
-		Just '+' -> inc bf
-		Just '-' -> dec bf
-		Just '.' -> bf
-		Just ',' -> bf
-		Just '[' -> jump bf
-		Just ']' -> jump bf
-		Just _   -> bf
+	if prgpointer bf == length (program bf)
+	then return ()
+	else case curSym bf of
+		'>' -> interpret . adv $ bf {pointer = pointer bf + 1}
+		'<' -> interpret . adv $ bf {pointer = pointer bf - 1}
+		'+' -> interpret . adv $ setCell (modChar (1) (curVal bf)) bf
+		'-' -> interpret . adv $ setCell (modChar (-1) (curVal bf)) bf
+		'.' -> do
+			putChar . curVal $ bf
+			hFlush stdout
+			interpret . adv $ bf
+		',' -> do
+			c <- getChar
+			interpret . adv $ setCell c bf
+		'[' -> interpret . adv $ jump bf
+		']' -> interpret . adv $ jump bf
+		_   -> interpret . adv $ bf
 
-instance Show BF where show = showBF
-showBF :: BF -> String
-showBF bf = reverse (dropWhile (== '\0') (reverse (array bf)))
+readBF :: String -> IO String
+readBF s = do
+	eof <- isEOF
+	if eof
+	then return s
+	else getChar >>= \c -> readBF . (s ++) $ [c]
 
 main :: IO ()
-main = getContents >>= putStrLn . show . interpret . newBF
+main = readBF "" >>= interpret . newBF >> putChar '\n'
